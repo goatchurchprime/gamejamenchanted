5cdadd158ccd9a411590ae1a93c248bcbcdc009d
Julian Todd <julian@goatchurch.org.uk>
2025-02-11 16:36:17 +0000

    moved candle into the tree blockout

--- enchantedtree/main.gd
+++ enchantedtree/main.gd
@@ -5,7 +5,11 @@ func getcontextmenutexts():
 
 func _on_radial_menu_menuitemselected(menutext):
 	if menutext == "togglecandle":
-		$CandleLight.visible = not $CandleLight.visible
+		$CandleLightIgloo.visible = not $CandleLightIgloo.visible
+		$CandleLightConetree.visible = not $CandleLightConetree.visible
 	elif menutext == "toggleshadow":
 		#$CandleLight.shadow_enabled = not $CandleLight.shadow_enabled
 		$WorldEnvironment/DirectionalLight3D.shadow_enabled = not $WorldEnvironment/DirectionalLight3D.shadow_enabled
+
+func _process(delta):
+	$CandleLightConetree.light_energy = clamp($CandleLightConetree.light_energy + randf_range(-4,4)*delta, 1, 3)
