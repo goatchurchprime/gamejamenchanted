bfd75b4ed05197d91fcb4982aa8f77fffa71bed9
Julian Todd <julian@goatchurch.org.uk>
2025-02-12 13:56:14 +0000

    add on fade collision

--- enchantedtree/main.gd
+++ enchantedtree/main.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
 func _ready() -> void:
-	pass
+	$XROrigin3D/XRAim3D/RadialMenu.connect("menuitemselected", _on_radial_menu_menuitemselected)
 
 func getcontextmenutexts():
-	return [ "togglecandle", "toggleshadow" ]
+	return [ "togglecandle", "toggleshadow", 
+			 "intotree" ]
 
 func _on_radial_menu_menuitemselected(menutext):
 	if menutext == "togglecandle":
@@ -13,6 +14,9 @@ func _on_radial_menu_menuitemselected(menutext):
 	elif menutext == "toggleshadow":
 		#$CandleLight.shadow_enabled = not $CandleLight.shadow_enabled
 		$WorldEnvironment/DirectionalLight3D.shadow_enabled = not $WorldEnvironment/DirectionalLight3D.shadow_enabled
+	#elif menutext == "intotree":
+		
+
 
 func _process(delta):
 	$CandleLightConetree.light_energy = clamp($CandleLightConetree.light_energy + randf_range(-4,4)*delta, 1, 3)
