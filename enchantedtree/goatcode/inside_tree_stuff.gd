e2889663328df4a1d05ac77cb1b20058143934a6
Julian Todd <julian@goatchurch.org.uk>
2025-02-14 19:18:22 +0000

    move the spawn points into the tree

---
+++ enchantedtree/goatcode/inside_tree_stuff.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+func _ready():
+	pass
+	#var inner_static_bodies = find_children("", "StaticBody3D", true)
+	#for inner_body:StaticBody3D in inner_static_bodies:
+	#	inner_body.set_collision_layer_value(1, false)
+	#	inner_body.set_collision_layer_value(8, true)
--- enchantedtree/main.gd
+++ enchantedtree/main.gd
@@ -74,7 +74,7 @@ func getyouintothetree():
 
 func getyoutothespawnpoint():
 	$Cockatrice.visible = false
-	$TaviBlockouts/BlockoutDepression/Cube.visible = true
+	$World/Enviroment/Terrain/EnchantedTreeSPLIT.visible = true
 	Ddebugmode = false
 	$XROrigin3D/PlayerBody.teleport(find_child("PosSpawnPoint").global_transform)
 	$WorldEnvironment/DirectionalLight3D.visible = true
@@ -99,12 +99,12 @@ func cock1shadow():
 func cock2attack():
 	$Cockatrice.visible = true
 	$Cockatrice/AnimationPlayer.play("move1")
-	$TaviBlockouts/BlockoutDepression/Cube.visible = false
+	$World/Enviroment/Terrain/EnchantedTreeSPLIT.visible = false
 	for s in [0.8, 0.6, 0.7, 0.5, 1.1]:
 		await get_tree().create_timer(s).timeout
 		$Cockatrice/AudioStreamPlayerCrunch.play()
 	await get_tree().create_timer(0.8).timeout
-	$TaviBlockouts/BlockoutDepression/Cube.visible = true
+	$World/Enviroment/Terrain/EnchantedTreeSPLIT.visible = true
 	$Cockatrice.visible = false
 	$Cockatrice/AnimationPlayer.stop()
 	
